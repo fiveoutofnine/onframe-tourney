388ac330fdbc58b32b3bda5f8d40c40af5817264
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-02-04 04:50:01 +0900

    chore: update

--- app/api/chess960/move/route.ts
+++ app/api/chess960/move/route.ts
@@ -213,3 +213,5 @@ export const GET = POST;
 // -----------------------------------------------------------------------------
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'default-no-store';
